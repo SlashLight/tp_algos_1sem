daee906d5c6d6810866f1804876e6533c5b302e0
slashlight <savvateev_andreyka@mail.ru>
2024-04-03 12:36:29 +0300

    task 5 completed

--- hw1/task5.cpp
+++ hw1/task5.cpp
@@ -36,7 +36,7 @@ void merge(T* arr, int p, int q, int r, bool compare(const T&, const T&) = isLes
     for (int j = 0; j < n2; ++j) {
         R[j] = arr[q + j + 1];
     }
-    
+
     int i = 0;
     int j = 0;
     int k = p;
@@ -71,105 +71,30 @@ void mergeSort(T* arr, int p, int r, bool compare(const T&, const T&)= isLessByD
     merge(arr, p, q, r, compare);
 }
 
-
-void run(istream& input, ostream& output) {
+int main() {
     int n;
-    input >> n;
-    userTimeline* a = new userTimeline[n];
+    cin >> n;
+    auto* a = new userTimeline[n];
     for (int i = 0; i < n; i++) {
-        int in;
-        int out;
-        input >> in >> out;
-        a[i] = userTimeline(in, out);
+        cin >> a[i].in >> a[i].out;
     }
 
     mergeSort(a, 0, n - 1, isLessByOutTime);
 
     int counter = 0;
+    int t2 = -1;
     int t1 = -1;
-    int t0 = -1;
 
     for (int i = 0; i < n; ++i) {
-        if (t1 < a[i].in) {
-            t1 = a[i].out;
-            t0 = a[i].out - 1;
+        if (t2 < a[i].in) {
+            t2 = a[i].out;
+            t1 = t2 - 1;
             counter += 2;
-        } else if (t0 < a[i].in) {
-            t0 = t1;
-            t1 = a[i].out;
+        } else if (t1 < a[i].in) {
+            t1 = t2;
+            t2 = a[i].out;
             counter++;
         }
     }
-    output << counter;
-}
-
-void testQueue() {
-    {
-        std::stringstream input;
-        std::stringstream output;
-        input << "5 1 10 10 12 1 10 1 10 23 24";
-        run(input, output);
-        assert(output.str() == "5");
-    }
-
-   {
-        std::stringstream input;
-        std::stringstream output;
-        input << "3 1 10 8 10 5 10";
-        run(input, output);
-        assert(output.str() == "2");
-    }
-
-    {
-        std::stringstream input;
-        std::stringstream output;
-        input << "5 7 9 5 7 18 20 12 18 9 12";
-        run(input, output);
-        assert(output.str() == "6");
-    }
-
-    {
-        std::stringstream input;
-        std::stringstream output;
-        input << "1 2 25";
-        run(input, output);
-        assert(output.str() == "2");
-    }
-
-    {
-        std::stringstream input;
-        std::stringstream output;
-        input << "5 7 9 25 30 15 17 20 23 1 4";
-        run(input, output);
-        assert(output.str() == "10");
-    }
-
-    {
-        std::stringstream input;
-        std::stringstream output;
-        input << "5 7 12 15 19 8 14 17 20 23 25";
-        run(input, output);
-        assert(output.str() == "6");
-    }
-
-    {
-        std::stringstream input;
-        std::stringstream output;
-        input << "0";
-        run(input, output);
-        assert(output.str() == "0");
-    }
-
-    {
-        std::stringstream input;
-        std::stringstream output;
-        input << "5 1 10 1 7 1 15 1 5 1 13";
-        run(input, output);
-        assert(output.str() == "2");
-    }
-}
-
-int main() {
-    testQueue();
-    return 0;
+    cout << counter;
 }
